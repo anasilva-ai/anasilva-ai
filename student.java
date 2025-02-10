2bdf7d0af5556090ace005987f6b189a4fe1796f
Anapaosc <158860842+anasilva-ai@users.noreply.github.com>
2025-02-09 19:33:23 -0600

    Create student.java

---
+++ student.java
@@ -0,0 +1,31 @@
+class student{
+    
+    string firstName;
+    string lastName;
+    int classYear;
+    string major;
+
+    student(string firstName, string lastName, int classYear, string major)
+    public static void main(String[] args){
+
+    public void display(){
+
+        system.out.println("FIRST NAME: "+firstName);
+        system.out.println("LAST NAME: "+lastName);
+        system.out.println("CLASS YEAR: "+classYear);
+        system.out.println("MAJOR: "+major);
+    }
+
+    }
+}
+
+public class sampleclass{
+
+        public class static void main(string[] args){
+
+                student s1=new student("paola,silva,2025,physics");
+            s1.display();
+
+        }
+}
+
